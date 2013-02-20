fb0e6497cfd3b67396aa8b274d26ef84acd680ed
Nixsy <nicholaslaidlaw60@gmail.com>
2013-02-20 04:07:36 +0000

    Updated version to  0.9.3

--- src/com/chopnix/ru/Repo.java
+++ src/com/chopnix/ru/Repo.java
@@ -11,8 +11,8 @@ public class Repo {
 	public final static String TEXTUREPACKS_XML = "https://dl.dropbox.com/u/27471347/ChopNix/texturepack.xml?dl=1";
 	public final static String CHANGELOG_FILE = "https://github.com/nixsy9/ChopNix-launcher/commits/master";
 	
-	public static final String VERSION = "0.9.2";
-	public static final int BUILD_NUMBER = 12;
+	public static final String VERSION = "0.9.3";
+	public static final int BUILD_NUMBER = 13;
 	
 	public final static String IRC_URL = "irc.chopnix.com";
 }
