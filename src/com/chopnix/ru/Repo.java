cca1b9119696355bf6b0707189b51c90d9ab531f
Nixsy <nicholaslaidlaw60@gmail.com>
2013-02-20 01:28:58 +0000

    Changed changelog location to github commits.

--- src/com/chopnix/ru/Repo.java
+++ src/com/chopnix/ru/Repo.java
@@ -9,7 +9,7 @@ public class Repo {
 	public final static String MODPACKS_XML = "https://dl.dropbox.com/u/27471347/ChopNix/devmodpacks.xml?dl=1";
 	public final static String VERSION_XML = "https://dl.dropbox.com/u/27471347/ChopNix/version.xml?dl=1";
 	public final static String TEXTUREPACKS_XML = "https://dl.dropbox.com/u/27471347/ChopNix/texturepack.xml?dl=1";
-	public final static String CHANGELOG_FILE = "https://dl.dropbox.com/u/27471347/ChopNix/news.txt";
+	public final static String CHANGELOG_FILE = "https://github.com/nixsy9/ChopNix-launcher/commits/master";
 	
 	public static final String VERSION = "0.9.1";
 	public static final int BUILD_NUMBER = 11;
