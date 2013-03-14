4838e2531e642af24ea9215c0919e96e54c11ad8
Nixsy <nicholaslaidlaw60@gmail.com>
2013-03-14 12:29:31 +0000

    New repo

--- src/com/chopnix/ru/Repo.java
+++ src/com/chopnix/ru/Repo.java
@@ -6,13 +6,13 @@ public class Repo {
 	public final static String MOJANG_STATE = "http://help.mojang.com/";
 	public final static String ONLINE_PHP = "http://chopnix.info/cc1/lcc1.php";
 	public final static String NEWS_FILE = "https://dl.dropbox.com/u/27471347/ChopNix/news.txt?dl=1";
-	public final static String MODPACKS_XML = "https://dl.dropbox.com/u/27471347/ChopNix/modpacks.xml?dl=1";
+	public final static String MODPACKS_XML = "https://dl.dropbox.com/u/27471347/ChopNix/chopnixmodz.xml?dl=1";
 	public final static String VERSION_XML = "https://dl.dropbox.com/u/27471347/ChopNix/version.xml?dl=1";
 	public final static String TEXTUREPACKS_XML = "https://dl.dropbox.com/u/27471347/ChopNix/texturepack.xml?dl=1";
 	public final static String CHANGELOG_FILE = "https://github.com/nixsy9/ChopNix-launcher/commits/master";
 
 	public static final String VERSION = "1.0.1";
-	public static final int BUILD_NUMBER = 16;
+	public static final int BUILD_NUMBER = 17;
 
 	public final static String IRC_URL = "irc.chopnix.com";
 }
